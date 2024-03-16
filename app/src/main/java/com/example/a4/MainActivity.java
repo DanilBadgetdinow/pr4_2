faf160ecd78b842c1d390706ea7a321d3ccc64ec
Danil <badgetninowdanil@gmail.com>
2024-03-16 09:53:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a4/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.a4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.staticButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container_view, new StaticFragment())
+                        .commit();
+            }
+        });
+
+        findViewById(R.id.dynamicButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container_view, new DynamicFragment())
+                        .commit();
+            }
+        });
+
+        findViewById(R.id.containerButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container_view, new ContainerFragment())
+                        .commit();
+            }
+        });
+    }
+}
